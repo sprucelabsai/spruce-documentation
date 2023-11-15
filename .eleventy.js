4e7df1b53853e14a00d11e3a9186d7b2146faaa5
talhafaisalglor <talhafaisalglor@gmail.com>
2023-11-15 05:28:18 +0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -21,8 +21,7 @@ module.exports = function (eleventyConfig) {
   // Markdown with anchor support
   eleventyConfig.setLibrary(
     'md',
-    markdownIt(mdOptions)
-      .use(markdownItAnchor, mdAnchorOpts)
+    markdownIt(mdOptions).use(markdownItAnchor, mdAnchorOpts)
   );
 
   // Table of contents plugin
@@ -64,4 +63,11 @@ module.exports = function (eleventyConfig) {
 
     return navigation;
   });
+
+  // Add this to specify the output directory
+  return {
+    dir: {
+      output: "docs"
+    }
+  };
 };
