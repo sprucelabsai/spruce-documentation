62c8f6589a5ee1bb7237f6e155bdde725e21df1f
Taylor Romero <me@taylorrome.ro>
2023-12-12 06:35:41 -0700

    patch: base path test

--- .eleventy.js
+++ .eleventy.js
@@ -5,25 +5,25 @@ const externalLinks = require("@aloskutov/eleventy-plugin-external-links");
 
 const registerExtensions = require("./11ty-extensions");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy({"./src/assets/": "assets"});
-  eleventyConfig.addPassthroughCopy({"./node_modules/font-awesome/css": "assets/font-awesome/css"});
-  eleventyConfig.addPassthroughCopy({"./node_modules/font-awesome/fonts": "assets/font-awesome/fonts"});
-  eleventyConfig.addPassthroughCopy({"./node_modules/highlight.js/styles/atom-one-light.min.css": "styles/highlight.js-atom-one-light.min.css"});
+  eleventyConfig.addPassthroughCopy({ "./src/assets/": "assets" });
+  eleventyConfig.addPassthroughCopy({ "./node_modules/font-awesome/css": "assets/font-awesome/css" });
+  eleventyConfig.addPassthroughCopy({ "./node_modules/font-awesome/fonts": "assets/font-awesome/fonts" });
+  eleventyConfig.addPassthroughCopy({ "./node_modules/highlight.js/styles/atom-one-light.min.css": "styles/highlight.js-atom-one-light.min.css" });
 
-  eleventyConfig.addPassthroughCopy({"./client-side-compiled/**/*": "scripts"});
-  eleventyConfig.addPassthroughCopy({"./styles-compiled/**/*": "styles"});
-  eleventyConfig.addPassthroughCopy({"./src/copy-to-root/*": "."});
+  eleventyConfig.addPassthroughCopy({ "./client-side-compiled/**/*": "scripts" });
+  eleventyConfig.addPassthroughCopy({ "./styles-compiled/**/*": "styles" });
+  eleventyConfig.addPassthroughCopy({ "./src/copy-to-root/*": "." });
 
- 
-  eleventyConfig.addPassthroughCopy({"./node_modules/lunr/lunr.min.js": "scripts/libs/lunr.min.js"});
+
+  eleventyConfig.addPassthroughCopy({ "./node_modules/lunr/lunr.min.js": "scripts/libs/lunr.min.js" });
 
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.setDataDeepMerge(false);
 
   eleventyConfig.addPlugin(EleventyRenderPlugin);
-  eleventyConfig.addPlugin(externalLinks, {overwrite: false});
+  eleventyConfig.addPlugin(externalLinks, { overwrite: false });
 
   eleventyConfig.setServerOptions({
     showAllHosts: true
@@ -34,6 +34,7 @@ module.exports = function(eleventyConfig) {
   registerExtensions(eleventyConfig);
 
   return {
+    pathPrefix: "/spruce-documentation/",
     dir: {
       input: "src/pages",
     }
