7f29ca54b586c8f8374da97fb539f20d71d9c273
Taylor Romero <me@taylorrome.ro>
2024-06-27 13:06:47 -0600

    patch: tweaks

--- src/client-side/index.ts
+++ src/client-side/index.ts
@@ -85,7 +85,7 @@ function setupNavHandlers() {
       clickedGrandchildLevel?.classList.toggle("open", !currentGrandchildStateOpen);
 
       // Update button arrow direction for grandchild level
-      btn.classList.toggle("up-arrow", currentGrandchildStateOpen);
+      btn.classList.toggle("up-arrow", !currentGrandchildStateOpen);
     });
   });
 }
