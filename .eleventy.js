6ec195ad850bd7dcb18b98f653e8d5c98d509f32
talhafaisalglor <talhafaisalglor@gmail.com>
2025-02-13 05:57:10 +0500

    patch: update to eleventy 3!

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,11 @@
-require("json5/lib/register");
+module.exports = async function (eleventyConfig) {
+  const { EleventyRenderPlugin } = await import("@11ty/eleventy");
+  const externalLinks = await import("@aloskutov/eleventy-plugin-external-links");
 
-const { EleventyRenderPlugin } = require("@11ty/eleventy");
-const externalLinks = require("@aloskutov/eleventy-plugin-external-links");
+  const registerExtensions = require("./11ty-extensions");
+  const fs = require("fs");
+  const path = require("path");
 
-const registerExtensions = require("./11ty-extensions");
-
-
-const fs = require('fs');
-const path = require('path');
-
-module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "./src/assets/": "assets" });
   eleventyConfig.addPassthroughCopy({ "./node_modules/font-awesome/css": "assets/font-awesome/css" });
   eleventyConfig.addPassthroughCopy({ "./node_modules/font-awesome/fonts": "assets/font-awesome/fonts" });
@@ -19,30 +15,29 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "./styles-compiled/**/*": "styles" });
   eleventyConfig.addPassthroughCopy({ "./src/copy-to-root/*": "." });
 
-
   eleventyConfig.addPassthroughCopy({ "./node_modules/lunr/lunr.min.js": "scripts/libs/lunr.min.js" });
 
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.setDataDeepMerge(false);
 
   eleventyConfig.addPlugin(EleventyRenderPlugin);
-  eleventyConfig.addPlugin(externalLinks, { overwrite: false });
+  eleventyConfig.addPlugin(externalLinks.default, { overwrite: false });
 
   eleventyConfig.setServerOptions({
-    showAllHosts: true
+    showAllHosts: true,
   });
 
   eleventyConfig.setWatchThrottleWaitTime(100);
 
   registerExtensions(eleventyConfig);
 
-  eleventyConfig.addShortcode("include_raw", function(file) {
+  eleventyConfig.addShortcode("include_raw", function (file) {
     const inputPath = this.page.inputPath;
     const inputDir = path.dirname(inputPath);
     const filePath = path.join(inputDir, file);
-    
+
     if (fs.existsSync(filePath)) {
-      return fs.readFileSync(filePath, 'utf8');
+      return fs.readFileSync(filePath, "utf8");
     } else {
       console.warn(`Warning: File not found - ${filePath}`);
       return `<!-- File not found: ${file} -->`;
@@ -52,7 +47,10 @@ module.exports = function (eleventyConfig) {
   return {
     pathPrefix: "/",
     dir: {
-      input: "src/pages",
-    }
-  }
+      input: "src/pages", // <== Change this so pages are placed correctly
+      includes: "../_includes",
+      data: "../_data",
+      output: "_site",
+    },
+  };
 };
--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,44 +1,40 @@
-const mainNav = require("./navigation/mainNav.json5");
-const headerNav = require("./navigation/headerNav.json5");
-const footerLinks = require("./navigation/footerLinks.json5");
+const mainNav = require("../_data/navigation/mainNav.json");
+const headerNav = require("../_data/navigation/headerNav.json");
+const footerLinks = require("../_data/navigation/footerLinks.json");
 
 function findTopLinkRecordFor(url, navLinks) {
-  return navLinks.find(link => {
-    if (link.url === url) { return true; }
-
+  return navLinks.find((link) => {
+    if (link.url === url) return true;
     if (link.children) {
-      return link.children.some(child => {
-        if (child.url === url) return true;
-
-        return child.children && child.children.some(grandchild => grandchild.url === url);
-      });
+      return link.children.some(
+        (child) => child.url === url || (child.children && child.children.some((grandchild) => grandchild.url === url))
+      );
     }
-
     return false;
   });
 }
 
 const mainNavObj = {
   ...mainNav,
-  links: mainNav.links.map(link => ({
+  links: mainNav.links.map((link) => ({
     ...link,
     hasChildren: link.children && link.children.length > 0,
-    children: link.children?.map(child => ({
+    children: link.children?.map((child) => ({
       ...child,
       hasChildren: child.children && child.children.length > 0,
-      children: child.children || []
-    }))
+      children: child.children || [],
+    })),
   })),
   getActiveParentLink(pageUrlRendering) {
     return findTopLinkRecordFor(pageUrlRendering, mainNav.links);
-  }
+  },
 };
 
 const headerNavObj = {
   ...headerNav,
   getActiveParentLink(pageUrlRendering) {
     return findTopLinkRecordFor(pageUrlRendering, headerNav.links);
-  }
+  },
 };
 
 const footerNavObj = {
@@ -48,5 +44,5 @@ const footerNavObj = {
 module.exports = {
   mainNav: mainNavObj,
   headerNav: headerNavObj,
-  footerNav: footerNavObj
+  footerNav: footerNavObj,
 };
